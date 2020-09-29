542b19302dc219a7ed19f5b56116d6646c442250
yorickov <yorickov@gmail.com>
2020-09-30 00:39:24 +0300

    add custom reactions

--- app/javascript/counter/CounterApp.jsx
+++ app/javascript/counter/CounterApp.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import { when, autorun } from 'mobx';
+
 import Counter from './Counter';
 import counterStore from './CounterStore';
+
+when(
+  () => counterStore.age > 26,
+  () => { alert('age is more that 26'); },
+);
+
+autorun(() => {
+  alert(`age is ${counterStore.age}`);
+}, {
+  name: 'Update',
+  delay: 1000,
+});
 
 const CounterApp = () => (
   <Counter store={counterStore} />
